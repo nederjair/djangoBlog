fc81e560b2fc7f4a2f191506894448e049253a9a
nederjair <nederjair@gmail.com>
2024-05-04 11:56:43 +0300

    added sqlite as the db in the settings

--- websiteRoot/website/settings.py
+++ websiteRoot/website/settings.py
@@ -77,12 +77,17 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': 'blogdb',
-        'USER': 'silly',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
+
+
+
+
 
 
 # Password validation
